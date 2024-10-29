523110a215eca39b1e5a0da00f45f289c51751f3
Colman Glagovich <cglagovich@tenstorrent.com>
2024-10-29 12:42:39 +0000

    Allow python>=3.8.10

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     url="https://github.com/meta-llama/llama-models",
     package_dir={"llama_models": "models"},
     classifiers=[],
-    python_requires=">=3.10",
+    python_requires=">=3.8.10",
     install_requires=read_requirements(),
     include_package_data=True,
 )
